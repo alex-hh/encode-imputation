1f6d123164468bfffc0d10fede71014c1b203192
alex-hh <alex.hhooker@gmail.com>
2020-04-07 09:57:51 +0100

    add kwarg to manually specify subset of tracks on which model was trained when running prediction single

--- prediction/build_prediction_single.py
+++ prediction/build_prediction_single.py
@@ -10,10 +10,10 @@ from prediction.save_bigwig import main as save_bigwig
 # pred_chroms = ['chr21'] # chromosomes to predict
 # pred_chroms = ['chr{}'.format(i) for i in list(range(23))+['X']]
 
-def main(model_name, expt_set, checkpoint_code, dataset='test', pred_chroms=['chr21']):
+def main(model_name, expt_set, checkpoint_code, dataset='test', train_dataset='all', pred_chroms=['chr21']):
   for pred_chrom in pred_chroms:
     print('Saving preds for model {} on chromosome {}'.format(model_name, pred_chrom))
-    save_model_chrom_preds(model_name, expt_set, pred_chrom, checkpoint_code, dataset=dataset)
+    save_model_chrom_preds(model_name, expt_set, pred_chrom, checkpoint_code, dataset=dataset, train_dataset=train_dataset)
     print('Evaluating preds')
     evaluate_preds(model_name, expt_set, pred_chrom, checkpoint_code)
 
@@ -30,6 +30,7 @@ if __name__ == '__main__':
   # parser.add_argument('-model_list', nargs='+', required=True)
   parser.add_argument('-chroms', nargs='+', default=['chr21'])
   parser.add_argument('--dataset', default='test')
+  parser.add_argument('--train_dataset', default='all')
   parser.add_argument('--expt_set', type=str, default='chr21_reprod') # name of folder in which model weights are saved, and in which predictions are to be saved
   args = parser.parse_args()
-  main(args.model_name, args.expt_set, args.checkpoint_code, dataset=args.dataset, pred_chroms=args.chroms)
+  main(args.model_name, args.expt_set, args.checkpoint_code, dataset=args.dataset, train_dataset=args.train_dataset, pred_chroms=args.chroms)
