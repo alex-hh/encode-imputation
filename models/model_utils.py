1037e60404aad5febc34ae4ec8a2a5321a1a3f7d
alex-hh <alex.hhooker@gmail.com>
2020-04-23 10:58:21 +0100

    fix

--- models/model_utils.py
+++ models/model_utils.py
@@ -36,7 +36,7 @@ def find_checkpoint(model_name, expt_set=None, checkpoint_code=None, moving_avg=
   if checkpoint_code is None:
     return find_last_checkpoint(model_name, expt_set, weights_dir, moving_avg=moving_avg)
   print('Searching for checkpoints in dir', weights_dir)
-  checkpoint_path = os.path.join(output_dir, 'weights', '' if expt_set is None else expt_set,
+  checkpoint_path = os.path.join(weights_dir, 'weights', '' if expt_set is None else expt_set,
                                  '{}_ep{}{}*.hdf5'.format(model_name, checkpoint_code, '-ewa' if moving_avg else ''))
   print('Searching for checkpoints matching', checkpoint_path)
   checkpoints = glob.glob(checkpoint_path)
